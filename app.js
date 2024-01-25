7973296ea5f0fc7d488912f7cb959ab167ff3ca5
ammarlouah <ammarlouah9@gmail.com>
2024-01-25 17:33:16 +0100

    setup express

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const PORT = 5000 || process.env.PORT
+
+app.get('/',(req,res)=>{
+    res.send("GoodBye World")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
