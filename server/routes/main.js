9363ffe8beb79bbafc237ff7f71ddaa8b701a215
ammarlouah <ammarlouah9@gmail.com>
2024-01-27 18:08:02 +0100

    Pagination

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,14 +4,25 @@ const Post = require('../models/Post')
 
 // Routes
 router.get('/', async (req,res)=>{
-    const locals = {
-        title : "NodeJs Blog",
-        desc : "This is a simple blog created by nodejs express and mongodb"
-    }
-
     try {
-        const data = await Post.find()
-        res.render('index',{locals , data})
+        const locals = {
+            title : "NodeJs Blog",
+            desc : "This is a simple blog created by nodejs express and mongodb"
+        }
+        let perPage = 5;
+        let page = req.query.page || 1;
+        let data = await Post.aggregate([{ $sort : {createdAt : 1}}])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+        res.render('index',{locals,
+            data,
+            current : page,
+            nextPage : hasNextPage ? nextPage : null
+        })
     } catch (error) {
         console.error(error);
     }
